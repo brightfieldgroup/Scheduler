dfbfe56a4b51c0a69a0b41625413a3194eb465bc
Nick Fausti <ncfausti@gmail.com>
2019-02-27 17:32:44 +0700

    updated print message to let user know how often running

--- src/scheduler.py
+++ src/scheduler.py
@@ -2,6 +2,7 @@ import schedule
 import time
 import job
 
+INTERVAL_IN_SECONDS = 30
 
 def run_for_n_seconds(n):
     schedule.every(n).seconds.do(job.run)
@@ -17,8 +18,8 @@ def run_for_n_seconds(n):
 # like: python scheduler.py
 # this will run whatever code is placed 
 # in job.run() once every second
-schedule.every(30).seconds.do(job.run)
-
+schedule.every(INTERVAL_IN_SECONDS).seconds.do(job.run)
+print('running job every %s seconds...' % (INTERVAL_IN_SECONDS))
 # schedule.every().hour.do(job.run)
 # schedule.every().day.at("22:17").do(job.run)
 
